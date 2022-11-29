877ce2471dff46e218e7a213726aecfbd54207a5
Miguel <mf.domingos@campus.fct.unl.pt>
2022-11-29 17:09:37 +0000

    Added prototype of FilterClass

---
+++ ganttproject/src/net/sourceforge/ganttproject/task/FilterClass.java
@@ -0,0 +1,11 @@
+package net.sourceforge.ganttproject.task;
+
+public class FilterClass {
+    private String restriction;
+
+    public FilterClass(String restriction) {
+        this.restriction = restriction;
+    }
+
+
+}
